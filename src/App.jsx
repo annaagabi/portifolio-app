05833ad09748e94b13a2d06facf9bde208185ffc
annaagabi <annaa0478@gmail.com>
2024-09-21 14:17:49 -0300

    Feat: header completo

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,25 @@ export default function App() {
 
   return (
     <div className={styles.portifolioContainer}>
+      <aside className={styles.asideMedia}>
+        <div className={styles.line}></div>
+        <a href="">
+          <img src={GitHubIcon} alt="Icone do github" />
+        </a>
+        <a href="">
+          <img src={DribbleIcon} alt="Icone do dribble" />
+        </a>
+        <a href="">
+          <img src={FigmaIcon} alt="Icone do figma" />
+        </a>
+      </aside>
+
       <header className={styles.header}>
         <div className={styles.headerTitle}>
           <img src={LogoDefault} alt="Imagem do portfolio logo" />
           <h1>Anna</h1>
         </div>
+
         <nav className={styles.nav}>
           <a href="">
             <p><span>#</span> home</p>
@@ -24,13 +38,16 @@ export default function App() {
           <a href="">
             <p><span>#</span> contacts</p>
           </a>
+
           <select>
             {/* <img src={ArrowDown} alt="icone de seta para baixo" /> */}
             <option>EN</option>
             <option>PT-BR</option>
           </select>
+
         </nav>
       </header>
+      <main></main>
     </div>
   );
 }

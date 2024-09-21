06794ec7a1677c356d778a2d8f22b706c924f302
annaagabi <annaa0478@gmail.com>
2024-09-21 13:32:51 -0300

    Feat: imagens utilizadas no header

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+export {default as ArrowDown} from './arrow-down.svg'
+export {default as LogoOutline} from './logo-outline.svg'
+export {default as LogoDefault} from './logo-default.svg'
+export {default as GitHubIcon} from './github.svg';
+export {default as DribbleIcon} from './dribble.svg';
+export {default as FigmaIcon} from './figma.svg';
